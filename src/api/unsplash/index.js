1f3247cd5c4552f1c333a0115186a8fd8a0040cf
Andres R. Bucheli <radio_seattle@hotmail.com>
2022-11-17 11:04:33 +0300

    test with image

--- src/api/unsplash/index.js
+++ src/api/unsplash/index.js
@@ -1,4 +1,4 @@
-const API_URL = `https://api.unsplash.com/search/photos?query=nature&page=1&per_page=5&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+const API_URL = `https://api.unsplash.com/search/photos?query=nature&page=1&per_page=5&client_id=cR8m7h0ADMIIfiDjXU-uGGCUZbu5VA_wqv7VR_hM5zM`;
 
 export const getImage = async () => {
   const response = await fetch(API_URL);
